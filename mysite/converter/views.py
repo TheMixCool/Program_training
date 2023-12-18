a5e75a180c379d3119bd1692072a16f58be15378
Egor <86021728+TheMixCool@users.noreply.github.com>
2023-12-18 10:20:31 +0300

    Update views.py

--- mysite/converter/views.py
+++ mysite/converter/views.py
@@ -6,12 +6,10 @@ from foo import string_to_video
 
 def page(request):
     text = request.GET.get('text')
-    speed = request.GET.get('speed')
     
     MyClass.objects.create(
-    	request = request.GET.get('text'),
-    	speed = request.GET.get('speed')
+    	request = request.GET.get('text')
     )
-    string_to_video(text,int(speed))
+    string_to_video(text)
     
     return render(request,'index.html')
