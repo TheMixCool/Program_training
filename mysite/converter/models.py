f4a191fabefc5a46a2aaf19ca7f10272ae6e66ae
Egor <86021728+TheMixCool@users.noreply.github.com>
2023-12-20 03:35:42 +0300

    Update models.py

--- mysite/converter/models.py
+++ mysite/converter/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class MyClass(models.Model):
     request = models.CharField(max_length=256, null=True)
-    speed = models.DecimalField(max_digits=100, decimal_places=3, null = True)
+    speed = models.CharField(max_length=4, null=True)
